0fe6a3158c81e367d619b6c094ed5aa4b7ea81f8
spox <spox@modspox.com>
2008-04-01 22:23:36 +0000

    Added the reply method

--- lib/mod_spox/Plugin.rb
+++ lib/mod_spox/Plugin.rb
@@ -15,5 +15,13 @@ module ModSpox
         def name
             self.class.name.to_s.gsub(/^.+:/, '')
         end
+        
+        # target:: target for message
+        # message:: string message
+        # This is a helper method that will send an outgoing Privmsg
+        # to the given target
+        def reply(target, message)
+            @pipeline << Messages::Outgoing::Privmsg.new(target, message)
+        end
     end
 end
