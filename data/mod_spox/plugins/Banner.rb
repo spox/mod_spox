8393aa3aadadee3e8e8ba3c3e9a0cf81f3557fed
spox <spox@modspox.com>
2008-04-07 22:11:10 +0000

    Timed bans now working

--- data/mod_spox/plugins/Banner.rb
+++ data/mod_spox/plugins/Banner.rb
@@ -15,8 +15,19 @@ class Banner < ModSpox::Plugin
         Signature.find_or_create(:signature => 'banmask (\S+) (\S+) (\d+) ?(.+)?', :plugin => name, :method => 'mask_ban', :group_id => admin.pk,
             :description => 'Kickban given mask from given channel for given number of seconds providing an optional message'
             ).params = [:mask, :channel, :time, :message]
+        @pipeline.hook(self, :mode_check, :Incoming_Mode)
         BanRecord.create_table unless BanRecord.table_exists?
         BanMask.create_table unless BanMask.table_exists?
+        @updater = nil
+        updater
+    end
+    
+    def destroy
+        updater(false)
+    end
+    
+    def mode_check(message)
+        Logger.log("RECIEVED MODE MESSAGE")
     end
     
     def default_ban(message, params)
@@ -34,22 +45,42 @@ class Banner < ModSpox::Plugin
         nick = Models::Nick.filter(:nick => params[:nick]).first
         channel = Channel.filter(:name => params[:channel]).first
         if(!me.is_op?(message.target))
-            reply(message.replyto "Error: I'm not a channel operator")
+            reply(message.replyto, "Error: I'm not a channel operator")
         elsif(!nick)
             reply(message.replyto, "#{message.source.nick}: Failed to find nick #{params[:nick]}")
         elsif(!channel)
             reply(message.replyto, "#{message.source.nick}: Failed to find channel #{params[:channel]}")
         elsif(nick)
-            mask = nick.source.empty? ? "#{nick.nick}!*@*" : "*!*@#{nick.address}" 
+            mask = nick.source.nil? ? "#{nick.nick}!*@*" : "*!*@#{nick.address}" 
             BanRecord.new(:nick_id => nick.pk, :bantime => params[:time].to_i, :remaining => params[:time].to_i,
                 :invite => false, :channel_id => channel.pk, :mask => mask).save
             message = params[:message] ? params[:message] : 'no soup for you!'
             @pipeline << ChannelMode.new(channel, '+b', mask)
             @pipeline << Kick.new(nick, channel, message)
+            updater
         end
     end
     
     def mask_ban(message, params)
+    end
+    
+    def updater(new_record = true)
+        unless @updater.nil?
+            time = Object::Time.now.to_i - @updater
+            BanRecord.filter{:remaining > 0}.update("remaining = remaining - #{time}")
+        end
+        BanRecord.filter{:remaining < 0 && :removed == false}.each do |record|
+            if(me.is_op?(record.channel))
+                @pipeline << ChannelMode.new(record.channel, '-b', record.mask)
+                record.set(:removed => true)
+                @pipeline << Invite.new(record.nick, record.channel) if record.invite
+            end
+        end
+        next_record = BanRecord.filter{:remaining > 0}.order(:remaining.ASC).first
+        if(next_record && new_record)
+            @updater = Object::Time.now.to_i
+            @pipeline << Messages::Internal::TimerAdd.new(self, next_record.remaining, true){ updater }
+        end
     end
     
     class BanRecord < Sequel::Model
@@ -60,6 +91,7 @@ class Banner < ModSpox::Plugin
             integer :remaining, :null => false, :default => 1
             text :mask, :null => false
             boolean :invite, :null => false, :default => false
+            boolean :removed, :null => false, :default => false
             foreign_key :channel_id, :null => false, :table => :channels
             foreign_key :nick_id, :null => false, :table => :nicks
         end
