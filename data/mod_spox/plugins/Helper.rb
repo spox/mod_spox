c5d7cc239f74998d9c167ae588bda32a8ef07ab9
spox <spox@modspox.com>
2008-04-29 19:57:42 +0000

    Fixed #94

--- data/mod_spox/plugins/Helper.rb
+++ data/mod_spox/plugins/Helper.rb
@@ -22,7 +22,12 @@ class Helper < ModSpox::Plugin
         if(sigs.count > 0)
             reply message.source, "Available triggers for plugin: \2#{params[:plugin]}\2"
             sigs.each do |sig|
-                reply message.source, "\2Pattern:\2 #{sig.signature} \2Parameters:\2 [#{sig.params.join(' | ')}] \2Description:\2 #{sig.description}"
+                output = []
+                output << "\2Pattern:\2 #{sig.signature}"
+                output << "\2Parameters:\2 [#{sig.params.join(' | ')}]" if sig.params
+                output << "\2Auth Group:\2 #{Group[sig.group_id].name}" if sig.group_id
+                output << "\2Description:\2 #{sig.description}" if sig.description
+                reply message.source, output.join(' ')
             end
         else
             reply message.replyto, "\2Error:\2 No triggers found for plugin named: #{params[:plugin]}"
