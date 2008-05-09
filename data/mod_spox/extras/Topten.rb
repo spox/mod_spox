71f492518767e37dca86f8ccc6e3e4bf064d4140
spox <spox@modspox.com>
2008-05-09 05:14:15 +0000

    Completed topten plugin

--- data/mod_spox/extras/Topten.rb
+++ data/mod_spox/extras/Topten.rb
@@ -9,25 +9,50 @@ class Topten < ModSpox::Plugin
             :requirement => 'public', :description => 'Show topten users since midnight')
         Signature.find_or_create(:signature => 'topten ([0-9]{4}\/[0-9]{2}\/[0-9]{2})', :plugin => name, :method => 'archive',
             :description => 'Show topten from given date', :requirement => 'public').params = [:date]
-        Signature.find_or_create(:signature => 'stats (\S+)', :plugin => name, :method => 'stats', :description => 'Show stats on nick',
+        Signature.find_or_create(:signature => 'stats ?(\S+)?', :plugin => name, :method => 'stats', :description => 'Show stats on nick',
             :requirement => 'public').params = [:nick]
-        Signature.find_or_create(:signature => 'stats lifetime (\S+)', :plugin => name, :method => 'life_stats',
+        Signature.find_or_create(:signature => 'stats lifetime? (\S+)?', :plugin => name, :method => 'life_stats',
             :description => 'Show stat totals for given nick', :requirement => 'public').params = [:nick]
         @pipeline.hook(self, :log_stats, :Incoming_Privmsg)
     end
     
     def topten(m, p)
+        stats = ChatStat.filter(:channel_id => m.target.pk, :daykey => construct_daykey).reverse_order(:bytes).limit(10)
+        if(stats.size > 0)
+            users = []
+            stats.each do |stat|
+                bytes = (stat.bytes > 1023) ? "#{sprintf('%.2f', (stat.bytes / 1024.0))}kb" : "#{stat.bytes}b"
+                users << "#{Nick[stat.nick_id].nick}: #{bytes}"
+            end
+            reply m.replyto, "\2Topten:\2 #{users.join(', ')}"
+        else
+            reply m.replyto, "\2Error:\2 No stats found for this channel."
+        end
+    end
+    
+    def archive(m, p)
+        stats = ChatStat.filter(:channel_id => m.target.pk, :daykey => p[:date]).reverse_order(:bytes).limit(10)
+        if(stats.size > 0)
+            users = []
+            stats.each do |stat|
+                bytes = (stat.bytes > 1023) ? "#{sprintf('%.2f', (stat.bytes / 1024.0))}kb" : "#{stat.bytes}b"
+                users << "#{Nick[stat.nick_id].nick}: #{bytes}"
+            end
+            reply m.replyto, "\2Topten:\2 #{users.join(', ')}"
+        else
+            reply m.replyto, "\2Error:\2 No stats found for date given."
+        end
     end
     
     def stats(m, p)
-        nick = Nick.locate(p[:nick], false)
+        nick = p[:nick] ? Nick.locate(p[:nick], false) : m.source
         if(nick.is_a?(Nick))
             stat = ChatStat.filter(:nick_id => nick.pk, :channel_id => m.target.pk, :daykey => construct_daykey).first
             if(stat)
                 bytes = (stat.bytes > 1023) ? "#{sprintf('%.2f', (stat.bytes / 1024.0))}kb" : "#{stat.bytes}b"
-                reply m.replyto, "#{p[:nick]}: #{bytes} logged. #{stat.words} words spoken. #{stat.questions} questions asked."
+                reply m.replyto, "#{nick.nick}: #{bytes} logged. #{stat.words} words spoken. #{stat.questions} questions asked."
             else
-                reply m.replyto, "\2Error:\2 #{p[:nick]} has no stats recorded for today"
+                reply m.replyto, "\2Error:\2 #{nick.nick} has no stats recorded for today"
             end
         else
             reply m.replyto, "\2Error:\2 Failed to find record of nick: #{p[:nick]}"
@@ -35,6 +60,14 @@ class Topten < ModSpox::Plugin
     end
     
     def life_stats(m, p)
+        nick = p[:nick] ? Nick.locate(p[:nick], false) : m.source
+        if(nick)
+            result = ChatStat.group(:nick_id).select(:SUM[:bytes] => :tbytes, :SUM[:words] => :twords, :SUM[:questions] => :tquestions).filter(:nick_id => nick.pk).first
+            bytes = (result.tbytes > 1023) ? "#{sprintf('%.2f', (result.tbytes / 1024.0))}kb" : "#{result.tbytes}b"
+            reply m.replyto, "#{nick.nick} (total): #{bytes} logged, #{result.twords} words spoken and #{result.tquestions} questions asked"
+        else
+            reply m.replyto, "\2Error:\2 Failed to find #{p[:nick]}"
+        end
     end
     
     def log_stats(m)
