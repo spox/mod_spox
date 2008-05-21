900d8d4d2953b01bb30c2020cf42ac3caf1d020e
spox <spox@modspox.com>
2008-05-21 03:30:04 +0000

    Modified to use Pool

--- lib/mod_spox/Logger.rb
+++ lib/mod_spox/Logger.rb
@@ -1,6 +1,16 @@
 module ModSpox
 
     class Logger
+    
+        def self.do_initialization
+            Logger.fd(nil) unless Logger.class_variable_defined?(:@@fd)
+            Logger.severity unless Logger.class_variable_defined?(:@@severity)
+            @@lock = Mutex.new unless Logger.class_variable_defined?(:@@lock)
+            @@lock.synchronize do
+                @@writer = LogWriter.new(@@fd) unless Logger.class_variable_defined?(:@@writer)
+                @@initialized = true
+            end
+        end
         
         # severity:: minimum severity for visible logging
         # Sets the maximum level of visible logs
@@ -21,9 +31,30 @@ module ModSpox
         # severity level of a message, the better chance it has of
         # being outputted
         def self.log(message, severity=1)
-            Logger.fd(nil) unless Logger.class_variable_defined?(:@@fd)
-            Logger.severity unless Logger.class_variable_defined?(:@@severity)
-            @@fd.puts("LOGGER [#{Time.now}]: #{message}") unless @@severity < severity
+            do_initialization unless Logger.class_variable_defined?(:@@initialized)
+            @@writer.log(message) unless @@severity < severity
+        end
+        
+    end
+    
+    class LogWriter < Pool
+    
+        attr_reader :fd
+        
+        def initialize(fd)
+            super()
+            @fd = fd
+            start_pool
+            puts "Log writer has been created and is ready to go"
+        end
+        
+        def log(message)
+            @queue << message
+        end
+        
+        def processor
+            message = @queue.pop
+            @fd.puts("LOGGER [#{Time.now}]: #{message}")
         end
         
     end
