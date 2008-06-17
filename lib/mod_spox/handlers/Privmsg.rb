5c333a9c7f5455a88ad24937054717dc46708d8c
spox <spox@modspox.com>
2008-06-17 16:58:23 +0000

    Cleaned up model saving

--- lib/mod_spox/handlers/Privmsg.rb
+++ lib/mod_spox/handlers/Privmsg.rb
@@ -13,20 +13,10 @@ module ModSpox
                     base_source = $1
                     source = find_model(base_source.gsub(/!.+$/, ''))
                     if(base_source =~ /!(.+)@(.+)$/)
-                        do_save = false
-                        unless(source.username == $1)
-                            source.username == $1
-                            do_save = true
-                        end
-                        unless(source.address == $2)
-                            source.address = $2
-                            do_save = true
-                        end
-                        unless(source.source == base_source)
-                            source.source = base_source
-                            do_save = true
-                        end
-                        source.save if do_save
+                        source.username == $1
+                        source.address = $2
+                        source.source = base_source
+                        source.save_changes
                     end
                     Models::NickChannel.find_or_create(:channel_id => target.pk, :nick_id => source.pk) if target.is_a?(ModSpox::Models::Channel)
                     return Messages::Incoming::Privmsg.new(string, source, target, message)
