e4129e6320adc0601ff118e02b3030c584eaa0ba
spox <spox@modspox.com>
2008-06-17 17:02:52 +0000

    Fixed to send output in proper order

--- data/mod_spox/plugins/Helper.rb
+++ data/mod_spox/plugins/Helper.rb
@@ -20,15 +20,17 @@ class Helper < ModSpox::Plugin
     def plugin_help(message, params)
         sigs = Signature.filter(:plugin => params[:plugin])
         if(sigs.count > 0)
-            reply message.source, "Available triggers for plugin: \2#{params[:plugin]}\2"
+            output = []
+            output << "Available triggers for plugin: \2#{params[:plugin]}\2"
             sigs.each do |sig|
-                output = []
-                output << "\2Pattern:\2 #{sig.signature}"
-                output << "\2Parameters:\2 [#{sig.params.join(' | ')}]" if sig.params
-                output << "\2Auth Group:\2 #{Group[sig.group_id].name}" if sig.group_id
-                output << "\2Description:\2 #{sig.description}" if sig.description
-                reply message.source, output.join(' ')
+                help = []
+                help << "\2Pattern:\2 #{sig.signature}"
+                help << "\2Parameters:\2 [#{sig.params.join(' | ')}]" if sig.params
+                help << "\2Auth Group:\2 #{Group[sig.group_id].name}" if sig.group_id
+                help << "\2Description:\2 #{sig.description}" if sig.description
+                output << help.join(' ')
             end
+            reply message.source, output
         else
             reply message.replyto, "\2Error:\2 No triggers found for plugin named: #{params[:plugin]}"
         end
