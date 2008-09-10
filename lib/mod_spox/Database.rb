6f62ced8a058a2a044e39bd489ec37b22e9d3ea9
spox <spox@modspox.com>
2008-09-10 00:50:35 +0000

    Added a reconnect method to re-establish a database connection

--- lib/mod_spox/Database.rb
+++ lib/mod_spox/Database.rb
@@ -1,34 +1,47 @@
 module ModSpox
 
     class Database
-    
-        def self.db=(database)
+
+        def Database.db=(database)
             @@db = nil
             @@db = database
         end
-        
-        def self.type=(type)
+
+        def Database.type=(type)
             @@type = nil
             @@type = type
         end
-        
-        def self.cache=(cache)
+
+        def Database.cache=(cache)
             @@cache = nil
             @@cache = cache
         end
-        
-        def self.cache
+
+        def Database.cache
             return Database.class_variable_defined?(:@@cache) ? @@cache : nil
         end
-        
-        def self.type
+
+        def Database.type
             return Database.class_variable_defined?(:@@type) ? @@type : nil
         end
-        
-        def self.db
+
+        def Database.db
             return Database.class_variable_defined?(:@@db) ? @@db : nil
         end
-    
+
+        def Database.reconnect
+            begin
+                @@db.test_connection
+                Logger.log('Database connection appears to be active. Closing.')
+                @@db.disconnect
+            rescue Object => boom
+                Logger.log('Database connection does not appear to be active.')
+            ensure
+                Logger.log('Reconnecting to database.')
+                ModSpox.setup_adapter
+            end
+        end
+
     end
 
 end
