5b90933912b96970e00abb36844f4fd1f01c0c30
spox <spox@modspox.com>
2008-09-16 00:18:18 +0000

    Added a Ban class to be used for communication on pipeline

--- data/mod_spox/plugins/Banner.rb
+++ data/mod_spox/plugins/Banner.rb
@@ -2,9 +2,9 @@ class Banner < ModSpox::Plugin
 
     include Models
     include Messages::Outgoing
-    
+
     def initialize(pipeline)
-        super(pipeline)
+        super
         admin = Group.find_or_create(:name => 'banner')
         Signature.find_or_create(:signature => 'ban (\S+)', :plugin => name, :method => 'default_ban', :group_id => admin.pk,
             :description => 'Kickban given nick from current channel').params = [:nick]
@@ -36,6 +36,7 @@ class Banner < ModSpox::Plugin
         @pipeline.hook(self, :mode_check, :Incoming_Mode)
         @pipeline.hook(self, :join_check, :Incoming_Join)
         @pipeline.hook(self, :who_check, :Incoming_Who)
+        @pipeline.hook(self, :process_ban, :Banner_Ban)
         BanRecord.create_table unless BanRecord.table_exists?
         BanMask.create_table unless BanMask.table_exists?
         BanNickExempt.create_table unless BanNickExempt.table_exists?
@@ -44,18 +45,22 @@ class Banner < ModSpox::Plugin
         load_timer
         @time = Object::Time.now
     end
-    
+
     def destroy
         reset_time
     end
-    
+
+    def process_ban(message)
+        ban(message.nick, message.channel, message.time, message.message, message.invite?, message.display_time?)
+    end
+
     def reset_time
         elapsed = Object::Time.now.to_i - @time.to_i
         BanRecord.filter('remaining > 0').update("remaining = remaining - #{elapsed}")
         BanMask.filter('bantime > 0').update("bantime = bantime - #{elapsed}")
         @time = Object::Time.now
     end
-    
+
     # message:: ModSpox::Messages::Incoming::Privmsg
     # params:: parameters
     # Perform a simple ban with default values
@@ -64,7 +69,7 @@ class Banner < ModSpox::Plugin
         params[:channel] = message.target.name
         full_ban(message, params)
     end
-    
+
     # message:: ModSpox::Messages::Incoming::Privmsg
     # params:: parameters
     # Ban given nick in given channel
@@ -72,7 +77,7 @@ class Banner < ModSpox::Plugin
         params[:time] = 86400
         full_ban(message, params)
     end
-    
+
     # message:: ModSpox::Messages::Incoming::Privmsg
     # params:: parameters
     # Ban nick in given channel for given time providing given message
@@ -89,7 +94,7 @@ class Banner < ModSpox::Plugin
             ban(nick, channel, params[:time], params[:message])
         end
     end
-    
+
     # nick:: ModSpox::Models::Nick to ban
     # channel:: ModSpox::Models::Channel to ban nick from
     # time:: number of seconds ban should last
@@ -126,7 +131,7 @@ class Banner < ModSpox::Plugin
             @pipeline << Kick.new(nick, channel, message)
         end
     end
-    
+
     # mask:: mask to match against source (Regexp)
     # channel:: ModSpox::Models::Channel to ban from
     # message:: kick message
@@ -141,7 +146,7 @@ class Banner < ModSpox::Plugin
         @pipeline << Messages::Internal::TimerAdd.new(self, record.remaining, nil, true){ record.destroy }
         reset_time
     end
-    
+
     # message:: ModSpox::Messages::Incoming::Privmsg
     # params:: parameters
     # Set a ban on any nick with a source match given regex
@@ -157,9 +162,9 @@ class Banner < ModSpox::Plugin
             end
         else
             reply(message.replyto, "Error: No record of channel: #{params[:channel]}")
-        end 
+        end
     end
-    
+
     # message:: ModSpox::Messages::Incoming::Privmsg
     # params:: parameters
     # List all ban masks
@@ -186,10 +191,10 @@ class Banner < ModSpox::Plugin
             reply(message.replyto, "\2Error:\2 Failed to find ban mask with ID: #{params[:id]}")
         end
     end
-    
+
     # message:: ModSpox::Messages::Incoming::Privmsg
     # params:: parameters
-    # List all currently active bans originating from the bot    
+    # List all currently active bans originating from the bot
     def ban_list(message, params)
         set = BanRecord.filter(:removed => false)
         if(set.size > 0)
@@ -203,7 +208,7 @@ class Banner < ModSpox::Plugin
             reply(message.replyto, "No bans currently active")
         end
     end
-    
+
     # message:: ModSpox::Messages::Incoming::Privmsg
     # params:: parameters
     # Remove given ban
@@ -213,9 +218,9 @@ class Banner < ModSpox::Plugin
             clear_record(record.pk, nil)
         else
             reply(message.replyto, "\2Error:\2 Failed to find ban record with ID: #{params[:id]}")
-        end 
+        end
     end
-    
+
     # Check all enabled ban masks and ban any matches found
     def check_masks
         masks = BanMask.map_masks
@@ -231,7 +236,7 @@ class Banner < ModSpox::Plugin
                     end
                     unless match.nil?
                         begin
-                            ban(nick, channel, match[:bantime], match[:message]) 
+                            ban(nick, channel, match[:bantime], match[:message])
                         rescue Object => boom
                             Logger.log("Mask based ban failed. Reason: #{boom}")
                         end
@@ -242,7 +247,7 @@ class Banner < ModSpox::Plugin
             end
         end
     end
-    
+
     # nick:: ModSpox::Models::Nick
     # channel:: ModSpox::Models::Channel
     # Check if the nick in the channel matches any ban masks
@@ -262,7 +267,7 @@ class Banner < ModSpox::Plugin
             end
         end
     end
-    
+
     # message:: ModSpox::Messages::Incoming::Mode
     # Check for mode changes. Remove pending ban removals if
     # done manually
@@ -283,19 +288,19 @@ class Banner < ModSpox::Plugin
             end
         end
     end
-    
+
     # message:: ModSpox::Messages::Incoming::Join
     # Check is nick is banned
     def join_check(message)
         mask_check(message.nick, message.channel)
     end
-    
+
     # message:: ModSpox::Messages::Incoming::Who
     # Check if we updated any addresses
     def who_check(message)
         check_masks
     end
-    
+
     def load_timer
         BanRecord.filter('removed = ? AND remaining > 0', false).each do |record|
             @pipeline << Messages::Internal::TimerAdd.new(self, record.remaining, nil, true){ clear_record(record.pk, record.remaining) }
@@ -304,7 +309,7 @@ class Banner < ModSpox::Plugin
             @pipeline << Messages::Internal::TimerAdd.new(self, record.bantime, nil, true){ record.destroy }
         end
     end
-    
+
     def clear_record(id, slept=nil)
         record = BanRecord[id]
         return if !record || record.removed
@@ -320,7 +325,7 @@ class Banner < ModSpox::Plugin
         end
         record.save
     end
-    
+
     # message:: ModSpox::Messages::Incoming::Privmsg
     # params:: parameters
     # Add ban exemption for a given mode
@@ -340,7 +345,7 @@ class Banner < ModSpox::Plugin
         end
         reply message.replyto, response
     end
-    
+
     # message:: ModSpox::Messages::Incoming::Privmsg
     # params:: parameters
     # Add ban exemption for a given nick
@@ -349,7 +354,7 @@ class Banner < ModSpox::Plugin
         nick = Helpers.find_model(params[:nick])
         unless(nick.is_a?(Models::Nick))
             reply message.replyto, "\2Error:\2 Failed to find record of: #{params[:nick]}"
-            return 
+            return
         end
         channel = params[:channel] ? Helpers.find_model(params[:channel]) : nil
         if(channel)
@@ -385,10 +390,10 @@ class Banner < ModSpox::Plugin
         end
         reply message.replyto, response
     end
-    
+
     # message:: ModSpox::Messages::Incoming::Privmsg
     # params:: parameters
-    # List given type of current ban exemptions    
+    # List given type of current ban exemptions
     def exempt_list(message, params)
         output = []
         if(params[:type] == 'nick')
@@ -444,7 +449,7 @@ class Banner < ModSpox::Plugin
         end
         reply message.replyto, response
     end
-    
+
     # nick:: ModSpox::Models::Nick
     # channel:: ModSpox::Models::Channel
     # Check if nick is currently exempt from bans
@@ -458,7 +463,7 @@ class Banner < ModSpox::Plugin
         end
         return false
     end
-    
+
     class BanRecord < Sequel::Model
         set_schema do
             primary_key :id
@@ -471,20 +476,20 @@ class Banner < ModSpox::Plugin
             foreign_key :channel_id, :null => false, :table => :channels
             foreign_key :nick_id, :null => false, :table => :nicks
         end
-        
+
         before_create do
             update_values(:stamp => Object::Time.now)
         end
-        
+
         def channel
             ModSpox::Models::Channel[channel_id]
         end
-        
+
         def nick
             ModSpox::Models::Nick[nick_id]
         end
     end
-    
+
     class BanMask < Sequel::Model
         set_schema do
             primary_key :id
@@ -494,11 +499,11 @@ class Banner < ModSpox::Plugin
             text :message
             foreign_key :channel_id, :null => false, :table => :channels
         end
-        
+
         def channel
             ModSpox::Models::Channel[channel_id]
         end
-        
+
         def self.map_masks
             masks = {}
             BanMask.all.each do |mask|
@@ -509,63 +514,96 @@ class Banner < ModSpox::Plugin
             return masks
         end
     end
-    
+
     class BanNickExempt < Sequel::Model
         set_schema do
             primary_key :id
             foreign_key :nick_id, :table => :nicks, :null => false
             foreign_key :channel_id, :table => :channels
         end
-        
+
         def nick
             return Models::Nick[nick_id]
         end
-        
+
         def channel
             return Models::Channel[channel_id]
         end
     end
-    
+
     class BanSourceExempt < Sequel::Model
         set_schema do
             primary_key :id
             varchar :source, :null => false
             foreign_key :channel_id, :table => :channels
         end
-        
+
         def channel
             return Models::Channel[channel_id]
         end
-        
+
         def mask
             return values[:source] ? Marshal.load(values[:source].unpack('m')[0]) : nil
         end
-        
+
         def mask=(val)
             update_values(:source => [Marshal.dump(val)].pack('m'))
         end
-        
+
     end
-    
+
     class BanModeExempt < Sequel::Model
         set_schema do
             primary_key :id
             varchar :mode, :null => false
             foreign_key :channel_id, :table => :channels, :unique => true
         end
-        
+
         def channel
             return Models::Channel[channel_id]
-        end        
+        end
     end
-    
+
     class NotOperator < Exceptions::BotException
     end
-    
+
     class NotInChannel < Exceptions::BotException
     end
-    
+
     class BanExemption < Exceptions::BotException
+    end
+
+    class Ban
+        # Nick to be kicked
+        attr_reader :nick
+        # Channel to be kicked from
+        attr_reader :channel
+        # Type of ban: :ban, :kick, :kickban
+        attr_reader :type
+        # Ban time (0 for no ban)
+        attr_reader :time
+        # Message to display in kick
+        attr_reader :message
+        def initialize(nick, channel, type=:kickban, message=nil, time=30, invite=false, display_time=false)
+            @nick = nick
+            @channel = channel
+            raise ModSpox::Exceptions::InvalidType.new('Ban requires a valid type (:ban, :kick, :kickban)') unless [:kick, :ban, :kickban].include?(type)
+            @type = type
+            @time = time.to_i
+            @message = message
+            @display_time = display_time ? true : false
+            @invite = invite ? true : false
+        end
+
+        # Display ban time in kick message
+        def display_time?
+            return @display_time
+        end
+
+        # Invite user to channel after ban expiration
+        def invite?
+            return @invite
+        end
     end
 
 end
