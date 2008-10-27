ae2810392cab6b69a0b4a9d5c2ff5e6fc56490d1
spox <spox@modspox.com>
2008-10-27 22:04:57 +0000

    Plugin for adding tickets to trac

---
+++ data/mod_spox/extras/TracTicket.rb
@@ -0,0 +1,68 @@
+require 'net/http'
+
+class TracTicket < ModSpox::Plugin
+    
+    def initialize(pipeline)
+        super
+        admin = Group.find_or_create(:name => 'admin')
+        add_sig(:sig => 'ticket (defect|enhancement|task) (.+?)( # (.+))?', :method => :add_ticket,
+                :desc => 'Add new ticket to trac ticket tracker', :params => [:type, :short, :trash, :long])
+        add_sig(:sig => 'ticket trac( (\S+))?', :method => :trac_location, :group => admin,
+                :desc => 'Show/set trac site', :params => [:trash, :site])
+        add_sig(:sig => 'ticket site', :method => :trac_location, :desc => 'Show trac site')
+        @site = Config[:trac]
+    end
+    
+    def trac_location(message, params)
+        if(params[:site])
+            site = Config.find_or_create(:name => 'trac')
+            site.value = params[:site]
+            site.save
+            @site = params[:site]
+            reply message.replyto, "\2Trac Update:\2 Location of trac system has been updated: #{params[:site]}"
+        else
+            output = "\2Error:\2 Trac site has not yet been set"
+            unless(@site.nil?)
+                output = "\2Trac location:\2 #{@site}"
+            end
+            reply message.replyto, output
+        end
+    end
+    
+    def add_ticket(message, params)
+        if(@site =~ /(http:\/\/)?([^\/:]+)(:\d+)?(.+)$/)
+            addr = $2
+            port = $3
+            path = $4
+            port = port.nil? ? 80 : port.gsub(/:/, '').to_i
+            con = Net::HTTP.new(addr, port)
+            begin
+                cookie, fid = get_form_info(con, path)
+                long = params[:long] ? params[:long] : params[:short]
+                headers = {
+                    'Cookie' => cookie,
+                    'Referer' => @site,
+                    'Content-Type' => 'application/x-www-form-urlencoded'
+                }   
+                data = "reporter=#{URI.escape(message.source.nick)}&summary=#{URI.escape(params[:short])}&description=#{URI.escape(long)}&type=#{URI.escape(params[:type])}&action=create&status=new&priority=minor&__FORM_TOKEN=#{URI.escape(fid)}"
+                resp, data = con.post(path, data, headers)
+                reply message.replyto, 'Ticket was successfully added to tracker'
+            rescue Object => boom
+                reply message.replyto, "\2Error:\2 Encountered unexpected error while submitting ticket: #{boom}"
+            end
+        else
+            reply message.replyto, "\2Error:\2 Trac location has not been set"
+        end
+    end
+    
+    def get_form_info(con, path)
+        res, content = con.get(path, {})
+        cookie = res['set-cookie']
+        if(content =~ /name="__FORM_TOKEN" value="(.+?)"/i)
+            return cookie, $1
+        else
+            raise 'Failed to locate proper identification'
+        end
+    end
+    
+end
