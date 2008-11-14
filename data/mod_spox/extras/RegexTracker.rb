95f4496230572a5a3de773d78afe5d574e67fdc1
spox <spox@modspox.com>
2008-11-14 23:57:14 +0000

    Completed tracking plugin

--- data/mod_spox/extras/RegexTracker.rb
+++ data/mod_spox/extras/RegexTracker.rb
@@ -6,7 +6,7 @@ class RegexTracker < ModSpox::Plugin
         track = Group.find_or_create(:name => 'track')
         add_sig(:sig => 'track info (\d+)', :method => :score, :desc => 'Show info on regex tracking',
                 :params => [:id])
-        add_sig(:sig => 'track list(\s \d+)', :method => :list, :desc => 'List currently tracked regex', :params => [:channel])
+        add_sig(:sig => 'track list(\s\S+)?', :method => :list, :desc => 'List currently tracked regex', :params => [:channel])
         add_sig(:sig => 'track add (.+)', :method => :add, :desc => 'Add new regex for tracking',
                 :params => [:regex], :group => track)
         add_sig(:sig => 'track remove (\d+)', :method => :remove, :desc => 'Remove regex from tracking',
@@ -20,14 +20,15 @@ class RegexTracker < ModSpox::Plugin
     end
     
     def check(message)
-        time = Time.now
         return unless @cache.has_key?(message.target.pk.to_i)
+        time = Time.now
         @cache[message.target.pk.to_i].each do |t|
-            if(message.message =~ /#{t}/)
+            if(message.message =~ /#{t[:regex]}/)
                 ti = TrackInfo[t[:id]]
-                if(ti.last_score.year != time.year || ti.yday != time.yday)
+                if(ti.last_score.year != time.year || ti.last_score.yday != time.yday)
                     ti.score_today = 0
                 end
+                ti.last_score = Time.now
                 ti.score_today += 1
                 ti.score_total += 1
                 ti.save
@@ -39,12 +40,12 @@ class RegexTracker < ModSpox::Plugin
         output = ''
         begin
             raise 'No channel given for list' if message.is_private? && !params[:channel]
-            channel = params[:channel] ? Helpers.find_model(params[:channel], false) : message.target.pk
+            channel = params[:channel] ? Helpers.find_model(params[:channel].strip, false) : message.target
             if(TrackInfo.filter(:channel_id => channel.pk).count > 0)
                 output = []
                 output << "\2Currently tracked regexs in #{channel.name}"
                 TrackInfo.filter(:channel_id => channel.pk).each do |t|
-                    output << "ID: #{t.pk} --- REGEX: #{t.regex}"
+                    output << "\2ID:\2 #{t.pk} --- \2REGEX:\2 #{t.regex}"
                 end
             else
                 output = 'Error: Nothing is currently being tracked'
@@ -63,6 +64,7 @@ class RegexTracker < ModSpox::Plugin
             t = TrackInfo.new
             t.regex = params[:regex]
             t.channel = message.target
+            t.last_score = Time.now
             t.save
             build_cache
             output = "New regex has been added for tracking (ID: #{t.pk})"
@@ -118,8 +120,9 @@ class RegexTracker < ModSpox::Plugin
     end
     
     def build_cache
-        @cache = []
+        @cache = {}
         TrackInfo.all.each do |t|
+            @cache[t.channel_id.to_i] = [] unless @cache.has_key?(t.channel_id.to_i)
             @cache[t.channel_id.to_i] << {:id => t.pk, :regex => t.regex}
         end
     end
@@ -127,11 +130,12 @@ class RegexTracker < ModSpox::Plugin
     class TrackInfo < Sequel::Model
         set_schema do
             primary_key :id, :null => false
-            varchar :regex, :null => false, :unique => true
+            varchar :regex, :null => false
             integer :score_total, :null => false, :default => 0
-            integer :score_day, :null => false, :default => 0
+            integer :score_today, :null => false, :default => 0
             timestamp :last_score, :null => false
             foreign_key :channel_id, :table => :channels, :null => false
+            index [:regex, :channel_id], :unique => true
         end
         
         def channel
@@ -139,17 +143,17 @@ class RegexTracker < ModSpox::Plugin
         end
         
         def channel=(chan)
-            raise InvalidType.new('Channel model was expected')
-            channel_id = chan.pk
+            raise Exceptions::InvalidType.new('Channel model was expected') unless chan.is_a?(Models::Channel)
+            values[:channel_id] = chan.pk
         end
         
         def regex=(r)
             r = [Marshal.dump(r)].pack('m')
-            super
+            super(r)
         end
         
         def regex
-            values[:regex] ? Marshal.load(values[:regex]).unpack('m')[0] : nil
+            values[:regex] ? Marshal.load(values[:regex].unpack('m')[0]) : nil
         end
     end
 
