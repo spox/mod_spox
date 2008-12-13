d6e1074f9b6269e4b8963badd697c98fc6bfc9c3
spox <spox@modspox.com>
2008-12-13 00:08:17 +0000

    Fixed signature

--- data/mod_spox/extras/Slashdot.rb
+++ data/mod_spox/extras/Slashdot.rb
@@ -4,9 +4,15 @@ require 'open-uri'
 class Slashdot < ModSpox::Plugin
 
     def initialize(pipeline)
+        begin
+            require 'htmlentities'
+        rescue Object => boom
+            Logger.warn('Error: This plugin requires the HTMLEntities gem. Please install and reload plugin.')
+            raise Exceptions::BotException.new("Missing required HTMLEntities library")
+        end
         super
-        add_sig(:sig => 'slashdot ?(\d+)?', :method => 'show_slashdot', :desc => 'Slashdot headlines', :params => [:num])
-        add_sig(:sig => '\/\. ?(\d+)?', :method => 'show_slashdot', :desc => 'Slashdot headlines', :params => [:num])
+        add_sig(:sig => '(slashdot|\/\.) ?(\d+)?', :method => 'show_slashdot', :desc => 'Slashdot headlines', :params => [:trig, :num])
+        @coder = HTMLEntities.new
     end
 
     def show_slashdot(message, params)
@@ -17,7 +23,10 @@ class Slashdot < ModSpox::Plugin
         output = []
         doc.elements.each('rdf:RDF/item') do |item|
             num -= 1
-            output << "\2/.\2 #{item.elements['title'].text} \2->\2 #{Helpers.tinyurl(item.elements['link'].text)}"
+            title = CGI::unescapeHTML(item.elements['title'].text)
+            title = title.gsub(/<.+?>/, ' ').gsub(/[\r\n]/, '').gsub(/\s+/, ' ')
+            title = @coder.decode(title)
+            output << "\2/.\2 #{title} \2->\2 #{Helpers.tinyurl(item.elements['link'].text)}"
             break if num < 1
         end
         reply message.replyto, output
