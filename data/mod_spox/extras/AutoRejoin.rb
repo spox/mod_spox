cd2cfdd4a0d2e1f6b2b9b8f353b7a29d3434aae8
spox <spox@modspox.com>
2008-12-17 02:27:48 +0000

    Added automatic channel rejoining

--- data/mod_spox/extras/AutoRejoin.rb
+++ data/mod_spox/extras/AutoRejoin.rb
@@ -3,11 +3,32 @@ class AutoRejoin < ModSpox::Plugin
     def initialize(pipeline)
         super
         @pipeline.hook(self, :check_kick, :Incoming_Kick)
+        @pipeline.hook(self, :check_join, :Incoming_Join)
+        @pipeline.hook(self, :check_part, :Incoming_Part)
+        @pipeline.hook(self, :do_joins, :Incoming_Welcome)
     end
     
     def check_kick(message)
         if(message.kickee == me)
             @pipeline << Messages::Outgoing::Join.new(message.channel)
+        end
+    end
+    
+    def check_join(m)
+        if(m.nick == me)
+            m.channel.update(:autojoin => true)
+        end
+    end
+    
+    def check_part(m)
+        if(m.nick == me)
+            m.channel.update(:autojoin => false)
+        end
+    end
+    
+    def do_joins(m)
+        Models::Channel.filter(:autojoin => true).each do |channel|
+            @pipeline << Messages::Outgoing::Join.new(channel)
         end
     end
 
