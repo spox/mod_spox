0e608efe3ca432f11bead10ee1f3dbd325607ab7
spox <spox@modspox.com>
2008-12-19 19:42:45 +0000

    New lmgtfy plugin

---
+++ data/mod_spox/extras/GoogleIt.rb
@@ -0,0 +1,13 @@
+class GoogleIt < ModSpox::Plugin
+    def initialize(pipeline)
+        super
+        add_sig(:sig => 'googleit (.+)', :method => :git, :description => 'Let me google that for you', :params => [:term])
+    end
+    
+    def git(m, params)
+        link = "http://www.letmegooglethatforyou.com/?q=#{URI.escape(params[:term])}"
+        output = ["Oh please, let me google: \2#{params[:term]}\2 for you."]
+        output << "#{link} -> #{Helpers.tinyurl(link)}"
+        reply m.replyto, output
+    end
+end
