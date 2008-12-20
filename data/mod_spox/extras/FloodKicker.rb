6858d8bb1737b81eb388287c3412846a9980cde2
spox <spox@modspox.com>
2008-12-20 00:34:49 +0000

    Completed plugin. Testing still required

--- data/mod_spox/extras/FloodKicker.rb
+++ data/mod_spox/extras/FloodKicker.rb
@@ -2,9 +2,12 @@ class FloodKicker
     def initialize(pipeline)
         super
         flood = Models::Auth.find_or_create(:name => 'flood')
+        @watched = []
         @channels = Setting.filter(:name => :floodkicker).first
-        @channels = {} if @channels.nil?
-        populate_chanhash
+        @channels = [] if @channels.nil?
+        @channels.each do |c|
+            @watched << c[:channel]
+        end
         @data = {}
         @pipeline.hook(self, :listener, :Incoming_Privmsg)
         add_sig(:sig => 'floodkick enable(\s \S+)?', :method => :add_channel, :group => flood,
@@ -47,7 +50,7 @@ class FloodKicker
         chan = get_chan(m.channel.pk)
         unless(chan.nil?)
             if(lines.nil? || lines == 0)
-                reply m.replyto, "Flood kicker lines for this channel currently set at: #{get_chan(m.channel.pk)[:lines]} lines"
+                reply m.replyto, "Flood kicker lines for this channel currently set at: #{chan[:lines]} lines"
             else
                 chan[:lines] = lines
                 save_channels
@@ -59,17 +62,55 @@ class FloodKicker
     end
     
     def max_seconds(m, params)
+        seconds = params[:seconds] ? params[:seconds].to_i : nil
+        chan = get_chan(m.channel.pk)
+        unless(chan.nil?)
+            if(seconds.nil? || seconds == 0)
+                reply m.replyto, "Flood kicker seconds for this channel currently set at: #{chan[:seconds]} seconds"
+            else
+                chan[:seconds] = seconds
+                save_channels
+                reply m.replyto, "Flood kicker seconds updated to: #{seconds} seconds"
+            end
+        else
+            reply m.replyto, "\2Error:\2 Flood kicker is not enabled in this channel"
+        end
     end
     
     def listener(m)
+        if(@watched.include?(m.channel.pk))
+            log_message(m)
+            if(kick_nick?(m.nick, m.channel))
+                chan = get_chan(m.channel.pk)
+                @pipeline << Messages::Outgoing::Kick.new(m.nick, m.channel, "Flooding. (exceeded #{chan[:lines]} lines in #{chan[:seconds]} seconds)")
+            end
+        end
     end
     
     private
+    
+    def log_message(m)
+        @data[m.channel.pk] = {} unless @data.has_key?(m.channel.pk)
+        @data[m.channel.pk][m.nick.pk] = [] unless @data[m.channel.pk].has_key?(m.nick.pk)
+        @data[m.channel.pk][m.nick.pk] << {:time => Time.now, :message => m.message}
+        @data[m.channel.pk][m.nick.pk].shift until @data[m.channel.pk][m.nick.pk].size <= get_chan(m.channel.pk)[:lines]
+    end
+    
+    def kick_nick?(nick, channel)
+        chan = get_chan(channel.pk)
+        if(@data[channel.pk].has_key?(nick.pk) && @data[channel.pk][nick.pk].size == chan[:lines])
+            return @data[channel.pk][nick.pk][@data[channel.pk][nick.pk].size - 1][:time].to_i - @data[channel.pk][nick.pk][0].to_i >= chan[:seconds].to_i
+        else
+            return false
+        end
+    end
     
     def save_channels
         s = Models::Setting.find_or_create(:name => 'floodkicker')
         s.value = @channels
         s.save
+        @watched.clear
+        @channels.each{|c| @watched << c.pk}
     end
     
     def watched_channels
