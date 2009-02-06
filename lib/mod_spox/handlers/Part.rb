64473ad9c55f23c56fe14e9c0cdfc9ce6f7c40cb
spox <spox@rubyforge.org>
2009-02-06 09:16:41 -0800

    Fixed part handler to make part message optional. Fixes #238

--- lib/mod_spox/handlers/Part.rb
+++ lib/mod_spox/handlers/Part.rb
@@ -6,11 +6,11 @@ module ModSpox
                 handlers[:PART] = self
             end
             def process(string)
-                if(string =~ /^:(\S+) PART (\S+) (.*)$/)
+                if(string =~ /^:(\S+) PART (\S+)( .+)?$/)
                     channel = find_model($2)
                     nick = find_model($1.gsub(/!.+$/, ''))
                     channel.nick_remove(nick)
-                    return Messages::Incoming::Part.new(string, channel, nick, $3)
+                    return Messages::Incoming::Part.new(string, channel, nick, $3.strip)
                 else
                     Logger.warn('Failed to parse PART message')
                     return nil
