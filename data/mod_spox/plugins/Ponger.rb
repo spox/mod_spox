01cc144f3fde6a6513ebd0b1adeefb5f4ecb5a0d
sine <spox@rubyforge.org>
2009-02-27 11:39:14 -0800

    Added lag sensor and auto reconnect

--- data/mod_spox/plugins/Ponger.rb
+++ data/mod_spox/plugins/Ponger.rb
@@ -2,13 +2,53 @@ class Ponger < ModSpox::Plugin
 
     def initialize(pipeline)
         super(pipeline)
+        @lag = nil
+        @last = nil
+        @attempts = 0
+        add_sig(:sig => 'lag', :method => :print_lag, :desc => 'Shows bot\'s current lag to server')
         @pipeline.hook(self, :ping, :Incoming_Ping)
+        @pipeline.hook(self, :get_lag_pong, :Incoming_Pong)
+        @pipeline << Messages::Internal::TimerAdd.new(self, 60){ send_lag_ping }
+        @lock = Mutex.new
+        send_lag_ping
     end
     
     # message:: ModSpox::Messages::Incoming::Ping
     # Sends responding pongs to server pings
     def ping(message)
         @pipeline << Messages::Outgoing::Pong.new(message.server, message.string)
+    end
+    
+    def send_lag_ping
+        if(@attempts > 0)
+            @pipeline << Messages::Internal::Disconnected.new
+            @lag = nil
+            @last = nil
+            @attempts = 0
+        else
+            t = Time.now
+            @last = t.to_f.to_s
+            @pipeline << Messages::Outgoing::Ping.new("#{@last}")
+            @attempts += 1
+        end
+    end
+    
+    def get_lag_pong(m)
+        @lock.synchronize do
+            t = m.string.to_f
+            return unless m.string == @last
+            @lag = Time.now.to_f - t
+            @last = nil
+            @attempts = 0
+        end
+    end
+    
+    def print_lag(m, params)
+        if(@lag.nil?)
+            warning m.replyto, 'Lag time currently unavailable'
+        else
+            information m.replyto, "Current lag time: #{sprintf('%0.4f', @lag)} seconds"
+        end
     end
 
 end
