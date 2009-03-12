aba320220a3cb632701cc2f1761623670c995164
sine <spox@rubyforge.org>
2009-03-11 18:17:07 -0700

    added category support to random

--- data/mod_spox/extras/FML.rb
+++ data/mod_spox/extras/FML.rb
@@ -5,14 +5,15 @@ class FML < ModSpox::Plugin
 
     def initialize(pipeline)
         super
-        add_sig(:sig => 'fml', :method => :random, :desc => 'Fuck my life')
+        add_sig(:sig => 'fml( (love|money|kids|work|health|sex|miscellaneous))?', :method => :random, :desc => 'Fuck my life', :params => [:wspace, :category])
         @site = 'http://api.betacie.com/view/'
         @vars = ['key=readonly', 'language=en']
     end
     
     def random(m, params)
         begin
-            reply m.replyto, get_text('random')
+            t = params[:category] ? "#{params[:category].strip}/" : ''
+            reply m.replyto, get_text("#{t}random")
         rescue Object => boom
             error m.replyto, 'Failed to fetch.' + boom.to_s
         end
@@ -23,12 +24,12 @@ class FML < ModSpox::Plugin
     def get_text(thing)
         data = Net::HTTP.get_response(URI.parse(@site+thing+'?'+@vars.join('&'))).body
         doc = REXML::Document.new(data)
-        output = nil
+        output = []
         doc.elements.each('root/items/item/text') do |t|
-            output = t.text
+            output << t.text
         end
-        raise 'Failed to fetch item from FML' if output.nil?
-        return output
+        raise 'Failed to fetch item from FML' if output.empty?
+        return output[rand(output.size)]
     end
 
 end
