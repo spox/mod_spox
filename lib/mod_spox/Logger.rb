327546eb3bec40c3c8ba970d91434d454e83be66
spox <spox@rubyforge.org>
2009-04-27 17:50:10 -0700

    fixed logging

--- lib/mod_spox/Logger.rb
+++ lib/mod_spox/Logger.rb
@@ -3,24 +3,30 @@ module ModSpox
 
     class Logger
         
-        def Logger.initialize(logger)
+        def Logger.initialize(logger, level)
+            l = {:info => ::Logger::INFO, :error => ::Logger::ERROR, :warn => ::Logger::WARN, :fatal => ::Logger::FATAL}
             @@log = logger.is_a?(::Logger) ? logger : nil
+            @@log.level = l[level]
         end
         
         def Logger.warn(s)
-            @@log.warn(s) unless @@log.nil?
+            @@log.warn(s) if Logger.log?
         end
         
         def Logger.info(s)
-            @@log.info(s) unless @@log.nil?
+            @@log.info(s) if Logger.log?
         end
         
         def Logger.fatal(s)
-            @@log.fatal(s) unless @@log.nil?
+            @@log.fatal(s) if Logger.log?
         end
         
         def Logger.error(s)
-            @@log.error(s) unless @@log.nil?
+            @@log.error(s) if Logger.log?
+        end
+        
+        def Logger.log?
+            Logger.class_variable_defined?(:@@log)
         end
         
     end
