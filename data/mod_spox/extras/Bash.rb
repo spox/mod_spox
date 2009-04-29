35d77075d2543d577c517bd07592100779fdd8b3
spox <spox@rubyforge.org>
2009-04-29 09:10:01 -0700

    modified regex. tested and working.

--- data/mod_spox/extras/Bash.rb
+++ data/mod_spox/extras/Bash.rb
@@ -9,7 +9,7 @@ class Bash < ModSpox::Plugin
 
     def initialize(pipeline)
         super
-        add_sig(:sig => 'bash( (top|latest))?', :method => :random, :desc => 'Bash quote fetcher', :params => [:wspace, :cmd])
+        add_sig(:sig => 'bash( (top|latest|#\d+))?', :method => :random, :desc => 'Bash quote fetcher', :params => [:wspace, :cmd])
         @site = 'http://bash.org/'
     end
     
@@ -17,13 +17,13 @@ class Bash < ModSpox::Plugin
         begin
             u = @site + '?top'
             if 'latest' == params[:cmd]
-              u = @site + 'latest'
+              u = @site + '?latest'
             elsif /^#(\d+)$/.match(params[:cmd])
               u = @site + '?' + $1
             end
             reply m.replyto, get_text(u)
         rescue Object => boom
-            error m.replyto, 'Failed to fetch.' + boom.to_s
+            error m.replyto, "Failed to fetch. #{boom}"
         end
     end
     
@@ -32,32 +32,20 @@ class Bash < ModSpox::Plugin
     def get_text(url)
         begin
             data = Net::HTTP.get_response(URI.parse(url)).body
-            if /\d$/.match(url)
-                # parse individual entries, they have a "qt" section within "quote"
-                m = data.scan(/(?:<p class="qt">.*)(&lt;(?:.*?\r\n)+.*?)<\/p>/s)
-            else
-                # all the quote lists use a "quote" section
-                m = data.scan(/(?:<p class="quote">.*)(&lt;(?:.*?\r\n)+.*?)<\/p>/s)
-            end
+            # parse individual entries, they have a "qt" section within "quote"
+            m = data.scan(/<p class="qt">(.*?)<\/p>/m)
+            raise 'No quotes found' unless m.length > 0
             i = rand(m.length)              # select random index
                                             # NOTE: this works fine for individuals, since rand(1) is always 0
-            q = m[i][0]                     # select a random quote
-            r = q.gsub("<br />\r\n", " ")   # replace newlines with spaces
-            s = CGI::unescapeHTML(r)        # unescape html entities
-            t = s.gsub("&nbsp;", " ")       # FIXME: for some reason this doesn't get escaped?(!)
-            u = t
-            [ # replace weird chars left in at least one of the quotes
-                [ "\x85", "..." ],
-                [ "\x92", "'"   ],
-                [ "\x93", "\""  ],
-                [ "\x94", "\""  ],
-            ].each{|x| u.gsub!(x[0], x[1]) }
-            v = u.gsub(/[\x80-\xFF]+/, "")  # nuke weird straggler chars
-            w = u.strip
-            x = w.gsub(/(<([^>]+)>(?:[^<]+))<\2>/){ $1 }  # replace duplicated name tags; condense lines
-            puts x
+            q = m[i][0].gsub(/<[^<]+>/, '') # select a random quote
+            q.gsub!("\r\n", " ")            # replace newlines with spaces
+            s = Helpers.convert_entities(q) # unescape html entities
+            s.strip!
+            s.gsub!(/(<([^>]+)>(?:[^<]+))<\2>/){ $1 }  # replace duplicated name tags; condense lines
+            return s
         rescue Object => boom
-            puts boom
+            Logger.error(boom)
+            raise boom
         end
     end
 end
