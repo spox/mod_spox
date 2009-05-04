8bd8465b282d733e26cec0a56fde799c1710f319
spox <spox@modspox.com>
2009-05-04 09:19:45 -0700

    fix on index create

--- lib/mod_spox/migrations/004_mode_index_fix.rb
+++ lib/mod_spox/migrations/004_mode_index_fix.rb
@@ -1,15 +1,15 @@
 module ModSpox
     module Migrations
-        class AuthRestructureModeFix < Sequel::Migration
+        class ModeIndexFix < Sequel::Migration
             
             def up
                 Database.db.drop_index :nick_modes, [:nick_id, :channel_id]
-                Database.db.create_index :nick_modes, [:nick_id, :channel_id], :unique => true
+                Database.db.add_index :nick_modes, [:nick_id, :channel_id], :unique => true
             end
             
             def down
                 Database.db.drop_index :nick_modes, [:nick_id, :channel_id]
-                Database.db.create_index :nick_modes, [:nick_id, :channel_id]
+                Database.db.add_index :nick_modes, [:nick_id, :channel_id]
             end
         end
     end
