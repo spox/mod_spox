8277cc17da145a794c904e36d682e1defba6e6a8
spox <spox@rubyforge.org>
2009-05-08 13:14:52 -0700

    names test and fixed names handler

--- lib/mod_spox/handlers/Names.rb
+++ lib/mod_spox/handlers/Names.rb
@@ -11,49 +11,54 @@ module ModSpox
      
             def process(string)
                 orig = string.dup
-                string.slice!(0..string.index(' '))
-                type = string.slice!(0..string.index(' ')-1)
-                if(type == RPL_NAMREPLY)
-                    3.times{ string.slice!(0..string.index(' ')) }
-                    chan = string.slice!(0..string.index(' ')-1)
-                    string.slice!(0..string.index(':'))
-                    @names[chan] = Array.new unless @names[chan]
-                    @raw[chan] = Array.new unless @raw[chan]
-                    @raw[chan] << orig
-                    @names[chan] += string.split
-                    return nil
-                else
-                    2.times{ string.slice!(0..string.index(' ')) }
-                    chan = string.slice!(0..string.index(' ')-1)
-                    channel = find_model(chan)
-                    @raw[chan] << orig if @raw[chan]
-                    nicks = Array.new
-                    ops = Array.new
-                    voice = Array.new
-                    raw = @raw[chan].dup if @raw[chan]
-                    @names[chan] = [] unless @names[chan].is_a?(Array)
-                    @names[chan].each do |n|
-                        nick = Models::Nick.find_or_create(:nick => n.gsub(/^[@+]/, ''))
-                        nick.visible = true
-                        nicks << nick
-                        if(n[0] == '@')
-                            ops << nick
-                            m = Models::NickMode.find_or_create(:nick_id => nick.pk, :channel_id => channel.pk)
-                            m.set_mode('o')
-                        elsif(n[0] == '+')
-                            voice << nick
-                            m = Models::NickMode.find_or_create(:nick_id => nick.pk, :channel_id => channel.pk)
-                            m.set_mode('v')
-                        else
-                            m = Models::NickMode.find_or_create(:nick_id => nick.pk, :channel_id => channel.pk)
-                            m.clear_modes
+                begin
+                    string.slice!(0..string.index(' '))
+                    type = string.slice!(0..string.index(' ')-1)
+                    if(type == RPL_NAMREPLY)
+                        3.times{ string.slice!(0..string.index(' ')) }
+                        chan = string.slice!(0..string.index(' ')-1)
+                        string.slice!(0..string.index(':'))
+                        @names[chan] = Array.new unless @names[chan]
+                        @raw[chan] = Array.new unless @raw[chan]
+                        @raw[chan] << orig
+                        @names[chan] += string.split
+                        return nil
+                    else
+                        2.times{ string.slice!(0..string.index(' ')) }
+                        chan = string.slice!(0..string.index(' ')-1)
+                        channel = find_model(chan)
+                        @raw[chan] << orig if @raw[chan]
+                        nicks = Array.new
+                        ops = Array.new
+                        voice = Array.new
+                        raw = @raw[chan].dup if @raw[chan]
+                        @names[chan] = [] unless @names[chan].is_a?(Array)
+                        @names[chan].each do |n|
+                            nick = Models::Nick.find_or_create(:nick => n.gsub(/^[@+]/, ''))
+                            nick.visible = true
+                            nicks << nick
+                            if(n[0] == '@')
+                                ops << nick
+                                m = Models::NickMode.find_or_create(:nick_id => nick.pk, :channel_id => channel.pk)
+                                m.set_mode('o')
+                            elsif(n[0] == '+')
+                                voice << nick
+                                m = Models::NickMode.find_or_create(:nick_id => nick.pk, :channel_id => channel.pk)
+                                m.set_mode('v')
+                            else
+                                m = Models::NickMode.find_or_create(:nick_id => nick.pk, :channel_id => channel.pk)
+                                m.clear_modes
+                            end
+                            channel.add_nick(nick)
                         end
-                        channel.add_nick(nick)
+                        check_visibility(nicks, channel)
+                        @names.delete(chan)
+                        @raw.delete(chan)
+                        return Messages::Incoming::Names.new(raw, channel, nicks, ops, voice)
                     end
-                    check_visibility(nicks, channel)
-                    @names.delete(chan)
-                    @raw.delete(chan)
-                    return Messages::Incoming::Names.new(raw, channel, nicks, ops, voice)
+                rescue Object
+                    Logger.error("Failed to parse NAMES message: #{orig}")
+                    return nil
                 end
             end
 
---
+++ tests/handlers/tc_Names.rb
@@ -0,0 +1,35 @@
+require "#{File.dirname(__FILE__)}/../BotHolder.rb"
+
+class TestNamesHandler < Test::Unit::TestCase
+
+    def setup
+        h = BotHolder.instance
+        @bot = h.bot
+        @test = {
+                 :names_start => ':swiftco.wa.us.dal.net 353 spox @ #mod_spox :@pizza_ pizza__ @mod_spox @spox +spex',
+                 :names_end => ':swiftco.wa.us.dal.net 366 spox #mod_spox :End of /NAMES list.',
+                 :bad => ':fubared 353 fail whale'
+                }
+    end
+
+    def test_expected
+        assert_equal('353', @bot.factory.find_key(@test[:names_start].dup))
+        assert_nil(@bot.factory.handlers[@bot.factory.find_key(@test[:names_start].dup)].process(@test[:names_start].dup))
+        assert_equal('366', @bot.factory.find_key(@test[:names_end].dup))
+        result = @bot.factory.handlers[@bot.factory.find_key(@test[:names_end].dup)].process(@test[:names_end].dup)
+        assert_kind_of(ModSpox::Messages::Incoming::Names, result)
+        assert_equal('#mod_spox', result.channel.name)
+        assert_equal(5, result.nicks.size)
+        assert_equal(3, result.ops.size)
+        assert_equal(1, result.voiced.size)
+        assert(ModSpox::Models::Nick.find_or_create(:nick => 'mod_spox').is_op?(result.channel))
+        assert(ModSpox::Models::Nick.find_or_create(:nick => 'pizza_').is_op?(result.channel))
+        assert(ModSpox::Models::Nick.find_or_create(:nick => 'spox').is_op?(result.channel))
+        assert(!ModSpox::Models::Nick.find_or_create(:nick => 'pizza__').is_op?(result.channel))
+        assert(ModSpox::Models::Nick.find_or_create(:nick => 'spex').is_voice?(result.channel))
+    end
+
+    def test_unexpected
+        assert_nil(@bot.factory.handlers[@bot.factory.find_key(@test[:bad].dup)].process(@test[:bad].dup))
+    end
+end
