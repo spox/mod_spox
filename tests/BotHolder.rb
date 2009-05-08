e1e35e30dbc60a50895efdbf36e142ab7b5bbffa
spox <spox@rubyforge.org>
2009-05-08 10:25:55 -0700

    set botnick

---
+++ tests/BotHolder.rb
@@ -0,0 +1,24 @@
+['sequel', 'thread', 'etc', 'getoptlong', 'logger'].each{|f|require f}
+# lets get sequel setup like we want it #
+require 'sequel/extensions/migration'
+Sequel::Model.plugin :validation_helpers
+Sequel::Model.plugin :schema
+Sequel::Model.unrestrict_primary_key
+require 'mod_spox/Version'
+require 'mod_spox/Loader'
+require 'test/unit'
+require 'singleton'
+
+class BotHolder
+    include Singleton
+    attr_reader :bot
+    def initialize
+        ModSpox::Database.db = nil
+        ModSpox.initialize_bot(Sequel.sqlite) # in memory db
+        require 'mod_spox/Bot'
+        @bot = ModSpox::Bot.new
+        m = ModSpox::Models::Nick.find_or_create(:nick => 'mod_spox')
+        m.update(:botnick => true)
+    end
+
+end
