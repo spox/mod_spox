3b26201c9b2183fff789c540d4ebd3a763913405
spox <spox@rubyforge.org>
2009-05-13 17:52:36 -0700

    fixed after testing

--- lib/mod_spox/handlers/Invite.rb
+++ lib/mod_spox/handlers/Invite.rb
@@ -8,12 +8,19 @@ module ModSpox
             # :spax!~spox@host INVITE spox :#m
             def process(string)
                 orig = string.dup
-                source = find_model(string.slice!(0..string.index('!')-1))
-                2.times{ string.slice!(0..string.index(' ')) }
-                target = find_model(string.slice!(0..string.index(' ')-1))
-                string.slice!(0..string.index(':'))
-                channel = find_model(string.strip)
-                return Messages::Incoming::Invite.new(orig, source, target, channel)
+                string = string.dup
+                begin
+                    string.slice!(0)
+                    source = find_model(string.slice!(0..string.index('!')-1))
+                    2.times{ string.slice!(0..string.index(' ')) }
+                    target = find_model(string.slice!(0..string.index(' ')-1))
+                    string.slice!(0..string.index(':'))
+                    channel = find_model(string.strip)
+                    return Messages::Incoming::Invite.new(orig, source, target, channel)
+                rescue Object
+                    Logger.error("Failed to parse INVITE message: #{orig}")
+                    return nil
+                end
             end
         end
     end
