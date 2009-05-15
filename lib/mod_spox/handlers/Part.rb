d829760f7a3772259420bc6cc8ec91862d274e88
spox <spox@rubyforge.org>
2009-05-15 15:54:13 -0700

    fixed and passes test

--- lib/mod_spox/handlers/Part.rb
+++ lib/mod_spox/handlers/Part.rb
@@ -9,18 +9,22 @@ module ModSpox
             # :mod_spox!~mod_spox@host PART #m :
             
             def process(string)
+                string = string.dup
                 orig = string.dup
-                string.slice!(0)
-                nick = find_model(string.slice!(0..string.index('!')-1))
-                2.times{ string.slice!(0..string.index(' ')) }
-                channel = find_model(string.slice!(0..string.index(' ')-1))
-                string.slice!(0..string.index(':'))
-                channel.remove_nick(nick)
-                channel.parked = false if nick.botnick == true
-                nick.visible = false if nick.chanels.empty?
-                nick.save_changes
-                channel.save_changes
-                return Messages::Incoming::Part.new(orig, channel, nick, string)
+                begin
+                    string.slice!(0)
+                    nick = find_model(string.slice!(0..string.index('!')-1))
+                    2.times{ string.slice!(0..string.index(' ')) }
+                    channel = find_model(string.slice!(0..string.index(' ')-1))
+                    string.slice!(0..string.index(':'))
+                    channel.remove_nick(nick)
+                    nick.visible = false if nick.channels.empty?
+                    nick.save_changes
+                    channel.save_changes
+                    return Messages::Incoming::Part.new(orig, channel, nick, string)
+                rescue Object
+                    Logger.error("Failed to parse PART message: #{orig}")
+                end
             end
         end
     end
