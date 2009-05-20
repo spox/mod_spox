c7adc0e78b7ab52969969617df73d7895a4ebe57
spox <spox@rubyforge.org>
2009-05-19 17:05:45 -0700

    fixed and tested

--- lib/mod_spox/handlers/Whois.rb
+++ lib/mod_spox/handlers/Whois.rb
@@ -16,86 +16,101 @@ module ModSpox
             
             def process(string)
                 orig = string.dup
-                until(string.slice(0..string.index(' ')-1) == RPL_WHOISUSER ||
-                        string.slice(0..string.index(' ')-1) == RPL_WHOISSERVER ||
-                        string.slice(0..string.index(' ')-1) == RPL_WHOISOPERATOR ||
-                        string.slice(0..string.index(' ')-1) == RPL_WHOISIDLE ||
-                        string.slice(0..string.index(' ')-1) == RPL_WHOISCHANNELS ||
-                        string.slice(0..string.index(' ')-1) == RPL_WHOISIDENTIFIED ||
-                        string.slice(0..string.index(' ')-1) == RPL_ENDOFWHOIS)
-                    string.slice!(0..string.index(' '))
-                end
-                case string.slice!(0..string.index(' ')-1)
-                when RPL_WHOISUSER
-                end
-                if(string =~ /#{RPL_WHOISUSER}\s\S+\s(\S+)\s(\S+)\s(\S+)\s\*\s:(.+)$/)
-                    key = $1
-                    nick = find_model($1)
-                    nick.username = $2
-                    nick.address = $3
-                    nick.real_name = $4
-                    nick.save_changes
-                    @cache[$1] = Messages::Incoming::Whois.new(nick)
-                    @cache[$1].raw_push(string)
-                    return nil
-                elsif(string =~ /#{RPL_WHOISCHANNELS}\s\S+\s(\S+)\s:(.+)$/)
-                    nick = $1
-                    @cache[nick] = Messages::Incoming::Whois.new(find_model(nick)) unless @cache[nick]
-                    @cache[$1].raw_push(string)
-                    $2.split(/\s/).each{|chan|
-                        channel = find_model(chan.gsub(/^[@\+]/, ''))
-                        channel.add_nick(nick)
-                        @cache[nick].channels_push(channel)
-                        if(chan[0].chr == '@')
-                            m = Models::NickMode.find_or_create(:nick_id => @cache[nick].nick.pk, :channel_id => channel.pk)
-                            m.set_mode('o')
-                        elsif(chan[0].chr == '+')
-                            m = Models::NickMode.find_or_create(:nick_id => @cache[nick].nick.pk, :channel_id => channel.pk)
-                            m.set_mode('v')
+                string = string.dup
+                begin
+                    until(string.slice(0..string.index(' ')-1) == RPL_WHOISUSER ||
+                            string.slice(0..string.index(' ')-1) == RPL_WHOISSERVER ||
+                            string.slice(0..string.index(' ')-1) == RPL_WHOISOPERATOR ||
+                            string.slice(0..string.index(' ')-1) == RPL_WHOISIDLE ||
+                            string.slice(0..string.index(' ')-1) == RPL_WHOISCHANNELS ||
+                            string.slice(0..string.index(' ')-1) == RPL_WHOISIDENTIFIED ||
+                            string.slice(0..string.index(' ')-1) == RPL_ENDOFWHOIS)
+                        string.slice!(0..string.index(' '))
+                    end
+                    case string.slice!(0..string.index(' ')-1)
+                        when RPL_WHOISUSER
+                            string.slice!(0)
+                            string.slice!(0..string.index(' '))
+                            nick = find_model(string.slice!(0..string.index(' ')-1))
+                            string.slice!(0)
+                            nick.username = string.slice!(0..string.index(' ')-1)
+                            string.slice!(0)
+                            nick.address = string.slice!(0..string.index(' ')-1)
+                            string.slice!(0..string.index(':'))
+                            nick.real_name = string
+                            nick.save_changes
+                            @cache[nick.nick] = Messages::Incoming::Whois.new(nick)
+                            @cache[nick.nick].raw_push(orig)
+                        when RPL_WHOISCHANNELS
+                            2.times{string.slice!(0..string.index(' '))}
+                            nick = find_model(string.slice!(0..string.index(' ')-1))
+                            @cache[nick.nick] = Messages::Incoming::Whois.new(nick) unless @cache[nick.nick]
+                            string.slice!(0..string.index(':'))
+                            string.split.each do |c|
+                                channel = find_model(['@','+'].include?(c.slice(0)) ? c.slice(1..c.size) : c)
+                                channel.add_nick(nick)
+                                @cache[nick.nick].channels_push(channel)
+                                if(c[0].chr == '@')
+                                    m = Models::NickMode.find_or_create(:nick_id => nick.pk, :channel_id => channel.pk)
+                                    m.set_mode('o')
+                                elsif(c[0].chr == '+')
+                                    m = Models::NickMode.find_or_create(:nick_id => nick.pk, :channel_id => channel.pk)
+                                    m.set_mode('v')
+                                else
+                                    m = Models::NickMode.find_or_create(:nick_id => nick.pk, :channel_id => channel.pk)
+                                    m.clear_modes
+                                end
+                            end
+                            @cache[nick.nick].raw_push(orig)
+                        when RPL_WHOISSERVER
+                            2.times{string.slice!(0..string.index(' '))}
+                            nick = find_model(string.slice!(0..string.index(' ')-1))
+                            string.slice!(0)
+                            @cache[nick.nick] = Messages::Incoming::Whois.new(nick) unless @cache[nick.nick]
+                            nick.connected_to = string.slice!(0..string.index(' ')-1)
+                            nick.save_changes
+                            @cache[nick.nick].raw_push(orig)
+                        when RPL_WHOISIDENTIFIED
+                            2.times{string.slice!(0..string.index(' '))}
+                            nick = find_model(string.slice!(0..string.index(' ')-1))
+                            @cache[nick.nick] = Messages::Incoming::Whois.new(nick) unless @cache[nick.nick]
+                            nick.auth.services_identified = true
+                            @cache[nick.nick].raw_push(orig)
+                        when RPL_WHOISIDLE
+                            2.times{string.slice!(0..string.index(' '))}
+                            nick = find_model(string.slice!(0..string.index(' ')-1))
+                            string.slice!(0)
+                            @cache[nick.nick] = Messages::Incoming::Whois.new(nick) unless @cache[nick.nick]
+                            nick.seconds_idle = string.slice!(0..string.index(' ')-1).to_i
+                            string.slice!(0)
+                            nick.connected_at = Time.at(string.slice!(0..string.index(' ')-1).to_i)
+                            nick.save_changes
+                            @cache[nick.nick].raw_push(orig)
+                        when RPL_WHOISOPERATOR
+                            2.times{string.slice!(0..string.index(' '))}
+                            nick = find_model(string.slice!(0..string.index(' ')-1))
+                            string.slice!(0)
+                            @cache[nick.nick] = Messages::Incoming::Whois.new(nick) unless @cache[nick.nick]
+                            @cache[nick.nick].raw_push(orig)
+                        when RPL_ENDOFWHOIS
+                            2.times{string.slice!(0..string.index(' '))}
+                            nick = find_model(string.slice!(0..string.index(' ')-1))
+                            @cache[nick.nick] = Messages::Incoming::Whois.new(nick) unless @cache[nick.nick]
+                            @cache[nick.nick].raw_push(orig)
+                            message = @cache[nick.nick]
+                            message.lock
+                            @cache.delete(nick.nick)
+                            return message
                         else
-                            m = Models::NickMode.filter(:nick_id => @cache[nick].nick.pk, :channel_id => channel.pk)
-                            m.clear_modes
-                        end
-                    }
+                            Logger.error("Failed to parse WHOIS type reply. Unknown part found: #{orig}")
+                    end
                     return nil
-                elsif(string =~ /#{RPL_WHOISSERVER}\s\S+\s(\S+)\s(\S+)\s:(.+)$/)
-                    nick = $1
-                    @cache[nick] = Messages::Incoming::Whois.new(find_model(nick)) unless @cache[nick]
-                    @cache[nick].nick.connected_to = $2
-                    @cache[nick].raw_push(string)
-                    return nil
-                elsif(string =~ /#{RPL_WHOISIDENTIFIED}\s\S+\s(\S+)\s/)
-                    nick = $1
-                    @cache[nick] = Messages::Incoming::Whois.new(find_model(nick)) unless @cache[nick]
-                    @cache[nick].nick.auth.services_identified = true
-                    @cache[nick].raw_push(string)
-                    return nil
-                elsif(string =~ /#{RPL_WHOISIDLE}\s\S+\s(\S+)\s(\d+)\s(\d+)\s:(.+?),(.+?)/)
-                    nick = $1
-                    @cache[nick] = Messages::Incoming::Whois.new(find_model(nick)) unless @cache[nick]
-                    @cache[nick].nick.seconds_idle = $2.to_i
-                    @cache[nick].nick.connected_at = Time.at($3.to_i)
-                    @cache[nick].raw_push(string)
-                    return nil
-                elsif(string =~ /#{RPL_WHOISOPERATOR}\s\S+\s(\S+)/)
-                    nick = $1
-                    @cache[nick] = Messages::Incoming::Whois.new(find_model(nick)) unless @cache[nick]
-                    @cache[nick].raw_push(string)
-                    return nil
-                elsif(string =~ /#{RPL_ENDOFWHOIS}\s\S+\s(\S+)\s:/)
-                    nick = $1
-                    @cache[nick] = Messages::Incoming::Whois.new(find_model(nick)) unless @cache[nick]
-                    @cache[nick].raw_push(string)
-                    message = @cache[nick]
-                    message.lock
-                    @cache[nick].nick.save
-                    @cache.delete(nick)
-                    return message
-                else
-                    Logger.warn('Failed to parse WHOIS type reply')
+                rescue Object
+                    Logger.error("Failed to parse WHOIS type reply: #{orig}")
                     return nil
                 end
             end
+
         end
     end
 end
