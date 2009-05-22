228082d7fd99e2632dc14572a9e647251cc98bc4
spox <spox@rubyforge.org>
2009-05-21 19:18:59 -0700

    added proper lookup

--- lib/mod_spox/models/Group.rb
+++ lib/mod_spox/models/Group.rb
@@ -10,6 +10,16 @@ module ModSpox
             many_to_many :auths, :join_table => :auth_groups, :class => 'ModSpox::Models::Auth'
             one_to_many :signature, :class => 'ModSpox::Models::Signature'
 
+            def Group.filter(args={})
+                args[:name].downcase! if args.has_key?(:name)
+                super(args)
+            end
+            
+            def Group.find_or_create(args={})
+                args[:name].downcase! if args.has_key?(:name)
+                super(args)
+            end
+
             def name=(group_name)
                 group_name.downcase!
                 super(group_name)
