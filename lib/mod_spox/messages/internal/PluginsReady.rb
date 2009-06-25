cf8a022ef934342d1e0e05e6968995602b63dc53
spox <spox@rubyforge.org>
2009-06-25 15:29:15 -0700

    internal message for ready notification

---
+++ lib/mod_spox/messages/internal/PluginsReady.rb
@@ -0,0 +1,10 @@
+module ModSpox
+    module Messages
+        module Internal
+            # Sends message to reload plugin. If new and stale
+            # attributes are set, only that plugin will be reloaded
+            class PluginsReady
+            end
+        end
+    end
+end
