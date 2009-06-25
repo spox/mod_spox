6be3f893745ed2f1ba97a4a18278350fa06fded1
spox <spox@rubyforge.org>
2009-06-25 12:30:31 -0700

    new tests

---
+++ tests/models/tc_Signature.rb
@@ -0,0 +1,14 @@
+require "#{File.dirname(__FILE__)}/../BotHolder.rb"
+require 'digest/sha1'
+
+class TestSignatureModel < Test::Unit::TestCase
+    def setup
+    end
+    
+    def teardown
+    end
+    
+    def test_signature
+    end
+
+end
