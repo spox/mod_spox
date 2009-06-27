71b22ea1fc87074ae573bf2e6d7d08d6236a82e6
spox <spox@rubyforge.org>
2009-06-27 11:29:43 -0700

    finally add nick changing ability

---
+++ data/mod_spox/plugins/Nicker.rb
@@ -0,0 +1,13 @@
+class Nicker < ModSpox::Plugin
+    def initialize(pipeline)
+        super(pipeline)
+        admin = Models::Group.filter(:name => 'admin').first
+        add_sig(:sig => 'nick (\S+)', :method => :change_nick, :group => admin, :params => [:nick])
+    end
+    
+    # message:: ModSpox::Messages::Incoming::Privmsg
+    # Join the given channel
+    def change_nick(message, params)
+        @pipeline << Messages::Outgoing::Nick.new(params[:nick])
+    end
+end
