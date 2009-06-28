b8091b78fa1975dc66ed9969de023327c2c389d2
spox <spox@rubyforge.org>
2009-06-28 12:52:25 -0700

    check for lack of ':' when no message

--- lib/mod_spox/handlers/Part.rb
+++ lib/mod_spox/handlers/Part.rb
@@ -7,6 +7,7 @@ module ModSpox
             end
 
             # :mod_spox!~mod_spox@host PART #m :
+            # :guy2net!~guy2net@92-239-14-145.cable.ubr15.haye.blueyonder.co.uk PART #php
             
             def process(string)
                 string = string.dup
@@ -15,8 +16,13 @@ module ModSpox
                     string.slice!(0)
                     nick = find_model(string.slice!(0..string.index('!')-1))
                     2.times{ string.slice!(0..string.index(' ')) }
-                    channel = find_model(string.slice!(0..string.index(' ')-1))
-                    string.slice!(0..string.index(':'))
+                    unless(string.index(' ').nil?)
+                        channel = find_model(string.slice!(0..string.index(' ')-1))
+                        string.slice!(0..string.index(':'))
+                    else
+                        channel = find_model(string)
+                        string = ''
+                    end
                     channel.remove_nick(nick)
                     nick.visible = false if nick.channels.empty?
                     nick.save_changes
