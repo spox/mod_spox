e2d8fef4ad1503c95e4d0773c8af33467dae87cc
spox <spox@modspox.com>
2009-06-30 14:58:47 -0700

    do downcase stuff

--- lib/mod_spox/models/Setting.rb
+++ lib/mod_spox/models/Setting.rb
@@ -11,6 +11,16 @@ module ModSpox
 
             #serialize(:value, :format => :marshal)
 
+            def find_or_create(args)
+                args[:name].downcase! if args[:name]
+                super(args)
+            end
+
+            def filter(args)
+                args[:name].downcase! if args[:name]
+                super(args)
+            end
+
             def name=(setting_name)
                 setting_name.downcase!
                 super(setting_name)
@@ -26,12 +36,14 @@ module ModSpox
             end
 
             def Setting.set(sym, value)
+                sym = sym.to_s.downcase
                 s = Setting.find_or_create(:name => "#{sym}")
                 s.value = value
                 s.save
             end
 
             def Setting.val(sym)
+                sym = sym.to_s.downcase
                 s = Setting.filter(:name => "#{sym}").first
                 return s ? s.value : nil
             end
