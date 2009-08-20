d2731dd579af1617d1979fac2e7e90893bd9a706
spox <spox@rubyforge.org>
2009-08-19 20:00:38 -0700

    defined basic filter structure

--- lib/mod_spox/Filter.rb
+++ lib/mod_spox/Filter.rb
@@ -1,8 +1,24 @@
 module ModSpox
     class Filter
-        # message:: ModSpox::Message type
+        # message type filter is applied to
+        attr_reader :type
+        def initialize(t)
+            @type = t
+        end
+
+        # message:: Message from pipeline
         # Applies filters to message
         def filter(message)
+            raise ArgumentError.new("Wrong type supplied (Expected: #{@type})") unless Helpers.type_of?(message, @type)
+            do_filter(message)
+        end
+        
+        protected
+        
+        # message:: Message to be filtered
+        # This is where the actual filtering takes place. This
+        # is the method to overload!
+        def do_filter(message)
             raise Exceptions::NotImplemented.new
         end
     end
