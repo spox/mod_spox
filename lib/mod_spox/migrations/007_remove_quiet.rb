2e8def59d409e73ac4d62e4b6cba0340cdfe46a8
spox <spox@modspox.com>
2009-09-10 11:23:22 -0700

    remove deprecated field

---
+++ lib/mod_spox/migrations/007_remove_quiet.rb
@@ -0,0 +1,12 @@
+module ModSpox
+    module Migrations
+        class Ignore < Sequel::Migration
+            def up
+                Database.db.drop_column :channels, :quiet
+            end
+            def down
+                Database.db.add_column :channels, :quiet, :bool
+            end
+        end
+    end
+end
