e1e419bdc6f3e54877435606420fff16a13aca1f
spox <spox@modspox.com>
2010-03-25 15:03:22 -0700

    fix up initialize and add a default destroy method

--- lib/mod_spox/Plugin.rb
+++ lib/mod_spox/Plugin.rb
@@ -11,10 +11,25 @@ module ModSpox
         # :pool:: ActionPool::Pool
         # Create new plugin instance
         def initialize(args={})
+            [:pipeline, :irc, :timer, :pool].each do |x|
+                unless(args.has_key?(x))
+                    raise ArgumentEror.new "Expecting #{x}"
+                end
+            end
             @pipeline = args[:pipeline]
             @irc = args[:irc]
             @timer = args[:timer]
             @pool = args[:pool]
+            @pluginmanager = args[:pm]
+        end
+
+        # Called before the the plugin is destroyed. Useful
+        # for cleaning up references that might be around of this
+        # instance so it will not hang around if the plugin is reloaded.
+        # NOTE: if overriding this method, call super or ensure you unhook
+        # self from the pipeline
+        def destroy
+            @pipeline.unhook(self)
         end
     end
 end
