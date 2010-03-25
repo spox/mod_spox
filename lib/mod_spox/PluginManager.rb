d1baa9ddc8d65f9065f68da80276baeb0409cb79
spox <spox@modspox.com>
2010-03-25 15:03:38 -0700

    add some actual implementation

--- lib/mod_spox/PluginManager.rb
+++ lib/mod_spox/PluginManager.rb
@@ -1,36 +1,115 @@
 require 'splib'
 Splib.load :CodeReloader, :Constants
 
+require 'mod_spox/Bot'
+
 module ModSpox
     # Manages plugins for mod_spox
     class PluginManager
+
+        # Hash of plugin instances and modules
+        attr_reader :plugins
+
+        # args:: Argument hash.
+        # :pipeline:: Pipeliner::Pipeline
+        # :irc:: BaseIRC::IRC
+        # :timer:: ActionTimer::Timer
+        # :pool:: ActionPool::Pool
         # Create a new plugin manager
         # :nodoc:
         # {:PluginName => {:module => Module(containing constants), :plugin => Plugin instance}}
-        def initialize
+        def initialize(args={})
+            [:pipeline, :irc, :timer, :pool].each do |x|
+                unless(args.has_key?(x))
+                    raise ArgumentEror.new "Expecting #{x}"
+                end
+            end
+            @args = args
+            @args[:pm] = self
             @plugins = {}
         end
-    end
 
-    def load_plugin
-    end
+        # args:: Arguments for loading ({:gem => gem_name} or {:file => path})
+        # Loads the plugin with the given name
+        def load_plugin(args = {})
+            plugs = []
+            if(args[:gem])
+                require args[:gem]
+                plugs = create_gem_plugins
+            elsif(args[:file])
+                plugs = load_plugins_in(args[:file])
+            else
+                raise ArgumentError.new 'Only :file or :gem types allowed'
+            end
+            plugs
+        end
 
-    def unload_plugin
-    end
+        # name:: Name of plugin
+        # Unloads the plugin with the given name
+        def unload_plugin(name)
+        end
 
-    def reload_plugins
-    end
+        # name:: Name of plugin (or :all to reload all plugins)
+        # Reloads single or all plugins
+        def reload_plugin(name = :all)
+        end
 
-    def find_plugins
-    end
+        def find_plugins
+            {:files => find_local_plugins,
+                :gems => find_gem_plugins}
+        end
 
-    def find_gem_plugins
+        private
 
-    end
+        # Instantiates any plugins found in the ModSpox::Plugins namespace
+        # that do not already exist. Returns array of plugin constants
+        # created.
+        def create_gem_plugins
+            plugs = ModSpox::Plugins.constants.map{|x|
+                ModSpox::Plugins.const_get(x)}.find_all{|x|
+                    x < ModSpox::Plugin && !@plugins.has_key(x)}
+            plugs.each do |pl|
+                @plugins[pl] = {:module => nil, :plugin => pl.new(@args)}
+            end
+            plugs
+        end
 
-    def find_local_plugins
-        if(File.exists?(ModSpox.config_dir) && File.directory?(ModSpox.config_dir))
-            Dir.glob("#{ModSpox.config_dir}/")
+        # file:: Path to file
+        # Load all plugins within given file
+        def load_plugins_in(file)
+            plugs = []
+            if(File.exists?(file))
+                mod = Splib.load_code(file)
+                plugs = mod.constants.map{|x|mod.const_get(x)}.find_all{|x|x < ModSpox::Plugin}
+                plugs.each do |pl|
+                    @plugins[pl] = {:module => mod, :plugin => pl}
+                end
+            else
+                raise ArgumentError.new 'File does not exist'
+            end
+            plugs
+        end
+
+        # Returns a list of all plugins found in local gem list
+        def find_gem_plugins
+            gems = []
+            begin
+                require 'rubygems'
+                dep = Gem::Dependency.new /^mod_spox_plugin/, nil
+                gems = Gem.source_index.search(dep)
+            rescue LoadError
+                # ignore
+            end
+            gems
+        end
+
+        # Find all local plugins in config directory
+        def find_local_plugins
+            files = []
+            if(File.exists?("#{ModSpox.config_dir}/plugins") && File.directory?("#{ModSpox.config_dir}/plugins"))
+                Dir.glob("#{ModSpox.config_dir}/plugins/*.rb"){|x| files << x}
+            end
+            files
         end
     end
 end
