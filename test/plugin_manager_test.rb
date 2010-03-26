8b7d1f54a79292636d9528abc6c20d02abaeb702
spox <spox@modspox.com>
2010-03-25 20:31:55 -0700

    add test for unload

--- test/plugin_manager_test.rb
+++ test/plugin_manager_test.rb
@@ -9,13 +9,21 @@ require 'mod_spox/PluginManager'
 class PluginManagerTest < Test::Unit::TestCase
     def setup
         @pm = ModSpox::PluginManager.new(:irc => nil, :pool => nil, :timer => nil, :pipeline => nil)
+        @dummy = File.dirname(__FILE__) + '/DummyPlugin.rb'
     end
 
     def test_file_load
-        assert_kind_of(Class, @pm.load_plugin(:file => File.dirname(__FILE__) + '/DummyPlugin.rb').first)
+        assert_kind_of(Class, @pm.load_plugin(:file => @dummy).first)
         assert_equal(1, @pm.plugins.size)
         assert(@pm.plugins[:Plug])
         assert_kind_of(ModSpox::Plugin, @pm.plugins[:Plug][:plugin])
         assert_kind_of(Module, @pm.plugins[:Plug][:module])
+    end
+
+    def test_file_unload
+        @pm.load_plugin(:file => @dummy)
+        assert(@pm.plugins[:Plug])
+        @pm.unload_plugin(:Plug)
+        assert(@pm.plugins.empty?)
     end
 end
