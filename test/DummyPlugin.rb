f6cdc866977584aec0c6398c76bafc8b4b62881f
spox <spox@modspox.com>
2010-03-25 21:05:40 -0700

    add var for testing

--- test/DummyPlugin.rb
+++ test/DummyPlugin.rb
@@ -1,4 +1,9 @@
 class Plug < ModSpox::Plugin
+    attr_accessor :var
+    def initialize(*args)
+        super
+        @var = nil
+    end
     # overload destroy and don't call
     # super so we don't have to actually
     # load up a pipeline
