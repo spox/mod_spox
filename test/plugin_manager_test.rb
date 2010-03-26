65d529677c14ed66ca45ddc94ce5efbe27931142
spox <spox@modspox.com>
2010-03-25 21:06:26 -0700

    add test for reloading

--- test/plugin_manager_test.rb
+++ test/plugin_manager_test.rb
@@ -26,4 +26,14 @@ class PluginManagerTest < Test::Unit::TestCase
         @pm.unload_plugin(:Plug)
         assert(@pm.plugins.empty?)
     end
+
+    def test_file_reload
+        @pm.load_plugin(:file => @dummy)
+        assert(@pm.plugins[:Plug])
+        @pm.plugins[:Plug][:plugin].var = :foo
+        assert_equal(:foo, @pm.plugins[:Plug][:plugin].var)
+        @pm.reload_plugin(:Plug)
+        assert_not_equal(:foo, @pm.plugins[:Plug][:plugin].var)
+        assert_nil(@pm.plugins[:Plug][:plugin].var)
+    end
 end
