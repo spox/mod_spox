4ddb28a831ca1f1f9b7ac534827fe3f8934c329b
spox <spox@modspox.com>
2010-04-18 08:44:58 -0700

    fix up some of the actual connection logic

--- lib/mod_spox/Bot.rb
+++ lib/mod_spox/Bot.rb
@@ -3,10 +3,13 @@ require 'actiontimer'
 require 'pipeliner'
 require 'spockets'
 require 'baseirc'
+require 'messagefactory'
 require 'pstore'
 require 'mod_spox'
 require 'mod_spox/Socket'
 require 'mod_spox/Outputter'
+require 'mod_spox/Logger'
+require 'mod_spox/PluginManager'
 
 module ModSpox
 
@@ -49,7 +52,8 @@ module ModSpox
             @pool = ActionPool::Pool.new(:a_to => 10)
             @timer = ActionTimer::Timer.new(:pool => @pool)
             @pipeline = Pipeliner::Pipeline.new(:pool => @pool)
-            @outputter = Outputter.new(@socket.queue)
+            @outputter = Outputter.new
+            @outputter.start
             @irc = BaseIRC::IRC.new(@outputter.queue)
             @sockets = Spockets::Spockets.new
             @factory = MessageFactory::Factory.new
@@ -61,42 +65,55 @@ module ModSpox
             unless(Thread.current == Thread.main)
                 raise 'This should only be called by the main thread'
             end
-            do_setup
-            @socket.connect
+            cycle_connect
+            Logger.debug 'Bot has been started. Main thread taking a nap until halt time.'
             @monitor.wait_until{ @halt }
+            Logger.debug 'Main thread back in action. Time to shut this thing down'
         end
 
         def stop
             @halt = true
-            @monitor.broadcast
             @pool.shutdown
+            close_socket
+            @monitor.broadcast
         end
 
         def connect_to(server, port=6667)
             close_socket
+            Logger.debug "Attempting connection to: #{server}:#{port}"
             @socket = Socket.new(:server => server, :port => port, :delay => @con_info[:burst_delay],
                 :burst_lines => @con_info[:burst_lines], :burst_in => @con_info[:burst_in])
+            @socket.connect
+            register_socket
+            Logger.debug "Established connection to: #{server}:#{port}"
         end
 
         def cycle_connect
-            load_servers if @con_info[:servers].empty?
+            load_servers if @con_info.empty? || @con_info[:servers].empty?
             srv = @con_info[:servers].pop
             connect_to(srv[:server], srv[:port])
+        end
+
+        def close_socket
+            return unless @socket
+            if(@socket.connected?)
+                @sockets.remove(@socket.socket)
+            end
+            @socket.stop
         end
 
         private
         
         # Gets everything setup to start processing messages. This is
         # pretty much what makes the bot go.
-        def do_setup
-            raise '' if @socket.nil?
-            @sockets.clear
+        def register_socket
             @sockets.add(@socket.socket) do |m|
                 m = @factory.process(m)
                 unless(m)
                     @pipeline << m
                 end
             end
+            @sockets.on_close(@socket.socket){ cycle_connect }
         end
 
         def load_servers
