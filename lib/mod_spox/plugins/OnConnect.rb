db7e16b2d6c7e545734e72a05c8c5bc4b0aa3a7b
spox <spox@modspox.com>
2010-04-19 16:56:56 -0700

    builtin plugin for establishing connection

---
+++ lib/mod_spox/plugins/OnConnect.rb
@@ -0,0 +1,29 @@
+module ModSpox
+    module Plugins
+        class OnConnect < ::ModSpox::Plugin
+            def setup
+                @pipeline.hook(Messages::Connected, self, :on_connect)
+                @nick = nil
+                @username = nil
+                @real_name = nil
+            end
+
+            def on_connect(message)
+                get_nick_info
+                @irc.nick(@nick)
+                @irc.user(@username, 8, @real_name)
+            end
+
+            private
+
+            def get_nick_info
+                info = PStore.new("#{ModSpox.config_dir}/bot.pstore")
+                info.transaction do
+                    @nick = info[:nick]
+                    @username = info[:username]
+                    @real_name = info[:realname]
+                end
+            end
+        end
+    end
+end
