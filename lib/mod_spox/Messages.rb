585c29eb63d6d172b9aa8cf730c47c63093b4b92
spox <spox@modspox.com>
2010-04-19 16:56:30 -0700

    internal message for pipline communications

---
+++ lib/mod_spox/Messages.rb
@@ -0,0 +1,12 @@
+module ModSpox
+    module Messages
+        class Message
+        end
+        class Connected < Message
+        end
+        class Disconnected < Message
+        end
+        class Shutdown < Message
+        end
+    end
+end
