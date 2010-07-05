e22aedc2ac3cf2c919c6b0081eeeba941b01d6c3
spox <spox@modspox.com>
2010-07-05 10:10:31 -0700

    Simple joiner plugin

---
+++ lib/mod_spox/plugins/Joiner.rb
@@ -0,0 +1,21 @@
+module ModSpox
+module Plugins
+class Joiner < ModSpox::Plugin
+    def setup
+        @pipeline.hook(Messages::Initialized, self, :add_triggers)
+    end
+
+    def destroy
+    end
+    
+    def add_triggers(m)
+        @pipeline << {:signature => {:regexp => /^join (\S+)$/, :call => {:object => self, :method => :join}, :matches => [:channel]}}
+        Logger.debug "Sent trigger registration information for joiner"
+    end
+
+    def join(m, args)
+        @irc.join args[:channel]
+    end
+end
+end
+end
